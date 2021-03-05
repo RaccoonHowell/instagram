ab98ea90fc2d322f805c1b4bdbc1d21901e2a563
Rachel <rac777blue@hotmail.com>
2021-03-05 17:57:25 +0000

    added button to login.js form

--- src/pages/login.js
+++ src/pages/login.js
@@ -49,6 +49,15 @@ export default function Login() {
                         onChange={({ target }) => setPassword(target.value)}
                         value={password}
                     />
+
+                    <button
+                        disabled={isInvalid}
+                        type="submit"
+                        className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+                        ${isInvalid && 'opacity-50'}`}
+                    >
+                    Login
+                    </button>
                 </form>
             </div>
         </div>
