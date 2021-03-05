7b747547a58c59e942a62ed4246f3b3222b22edb
Rachel <rac777blue@hotmail.com>
2021-03-05 17:52:21 +0000

    added password field to login.js form

--- src/pages/login.js
+++ src/pages/login.js
@@ -33,12 +33,21 @@ export default function Login() {
 
                 <form onSubmit={handleLogin} method='POST'>
                     <input
-                    aria-label='Enter your email address'
-                    type='text'
-                    placeholder='Email address'
-                    className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
-                    onChange={({ target }) => setEmailAddress(target.value)}
-                    value={emailAddress}
+                        aria-label='Enter your email address'
+                        type='text'
+                        placeholder='Email address'
+                        className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+                        onChange={({ target }) => setEmailAddress(target.value)}
+                        value={emailAddress}
+                    />
+
+                    <input
+                        aria-label='Enter your password'
+                        type='password'
+                        placeholder='Password'
+                        className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+                        onChange={({ target }) => setPassword(target.value)}
+                        value={password}
                     />
                 </form>
             </div>
