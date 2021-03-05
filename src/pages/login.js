cbe7c345b1eda6909e229b6fe2b8f2de0ea1ccc9
Rachel <rac777blue@hotmail.com>
2021-03-05 17:47:26 +0000

    created form on login.js and added email address field to it

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 export default function Login() {
@@ -22,6 +22,25 @@ export default function Login() {
         <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
             <div className='flex w-3/5'>
                 <img src='/images/iphone-with-profile.jpg' alt='iPhone with Instagram app' />
+            </div>
+
+            <div className='flex flex-col w-2/5'>
+                <h1 className='flex justify-center w-full'>
+                    <img src='/images/logo.png' alt='Instagram' className='mt-2 w-6/12 mb-4' />
+                </h1>
+
+                {error && <p className='mb-4 text-xs text-red-primary'>{error}</p>}
+
+                <form onSubmit={handleLogin} method='POST'>
+                    <input
+                    aria-label='Enter your email address'
+                    type='text'
+                    placeholder='Email address'
+                    className='text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2'
+                    onChange={({ target }) => setEmailAddress(target.value)}
+                    value={emailAddress}
+                    />
+                </form>
             </div>
         </div>
     );
